e9ffd69a92849236c753c637732c5fd0dad965cc
muhiro_ <muhiro.12@gmail.com>
2021-12-29 16:50:52 +0900

    Remove decimal

---
+++ Incomes/Logic/Extension/NSDecimalNumberExtension.swift
@@ -0,0 +1,25 @@
+//
+//  NSDecimalNumberExtension.swift
+//  Incomes
+//
+//  Created by Hiromu Nakano on 2020/06/24.
+//  Copyright © 2020 Hiromu Nakano. All rights reserved.
+//
+
+import Foundation
+
+extension NSDecimalNumber {
+    var asCurrency: String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        guard let currency = formatter.string(for: self) else {
+            assertionFailure()
+            return .empty
+        }
+        return currency
+    }
+
+    var asMinusCurrency: String {
+        return "-" + asCurrency
+    }
+}
