06ac39691aa05b61c989046bbd308b2efef007da
muhiro_ <muhiro.12@gmail.com>
2022-01-15 17:58:31 +0900

    Update EnvironmentParameter

--- Incomes/Logic/Model/EnvironmentParameter.swift
+++ Incomes/Logic/Model/EnvironmentParameter.swift
@@ -9,27 +9,29 @@
 import Foundation
 
 struct EnvironmentParameter {
-    static var productID: String {
+    private init() {}
+
+    static var productID: String = {
         #if DEBUG
         return Secret.productIDDev.rawValue
         #else
         return Secret.productID.rawValue
         #endif
-    }
+    }()
 
-    static var revenueCatAPIKey: String {
+    static var revenueCatAPIKey: String = {
         #if DEBUG
         return Secret.revenueCatAPIKeyDev.rawValue
         #else
         return Secret.revenueCatAPIKey.rawValue
         #endif
-    }
+    }()
 
-    static var admobBannerID: String {
+    static var admobBannerID: String = {
         #if DEBUG
         return Secret.admobBannerIDDev.rawValue
         #else
         return Secret.admobBannerID.rawValue
         #endif
-    }
+    }()
 }
