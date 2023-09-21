3040937c9edf3e7787126327727a189297545c62
muhiro_ <muhiro.12@gmail.com>
2023-09-21 17:38:59 +0900

    Update debug option

--- Incomes/Presentation/View/DebugView.swift
+++ Incomes/Presentation/View/DebugView.swift
@@ -46,7 +46,7 @@ extension DebugView: View {
         }
         .alert(String.debugSetPreviewData, isPresented: $isAlertPresented) {
             Button(.localized(.cancel), role: .cancel) {}
-            Button(String.debugOK) {
+            Button(String.debugOK, role: .destructive) {
                 do {
                     let items = try PreviewData.items(context: context)
                     items.forEach(context.insert)
