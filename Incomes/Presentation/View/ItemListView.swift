2a7d849970ca7c509d888673be93c782847e9be6
muhiro_ <muhiro.12@gmail.com>
2023-09-26 17:56:06 +0900

    Localize year and yearMonth

--- Incomes/Presentation/View/ItemListView.swift
+++ Incomes/Presentation/View/ItemListView.swift
@@ -18,7 +18,7 @@ struct ItemListView {
     private let predicateBuilder: (Tag) -> Predicate<Item>
 
     init(tag: Tag, predicateBuilder: @escaping (Tag) -> Predicate<Item>) {
-        self.title = tag.name
+        self.title = tag.displayName
         self.yearTags = Set(
             tag.items?.compactMap {
                 $0.tags?.first {
