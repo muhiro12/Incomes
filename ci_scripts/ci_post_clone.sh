b3d61798dbeb8bf4d3e0f7b60079d37c411eeb5d
muhiro_ <muhiro.12@gmail.com>
2024-03-24 00:29:27 +0900

    Add scripts for Xcode Cloud

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Incomes
+#
+#  Created by Hiromu Nakano on 2024/03/24.
+#  Copyright © 2024 Hiromu Nakano. All rights reserved.
+
+mkdir Incomes/Configuration/Package/Firebase/
+echo '${{ secrets.GOOGLESERVICE_BASE64 }}' | base64 -d -o Incomes/Configuration/Package/Firebase/GoogleService-Info.plist
+echo '${{ secrets.SECRET_BASE64 }}' | base64 -d -o Incomes/Configuration/Secret.swift
+echo '${{ secrets.STOREKIT_BASE64 }}' | base64 -d -o Incomes/Configuration/Package/StoreKit/StoreKitTestCertificate.cer
