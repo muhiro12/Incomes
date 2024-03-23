72181b30ec5dfe8f700835ea1c17ed23a83f9f32
muhiro_ <muhiro.12@gmail.com>
2024-03-24 00:55:31 +0900

    Modify path in ci scripts

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,7 +6,7 @@
 #  Created by Hiromu Nakano on 2024/03/24.
 #  Copyright © 2024 Hiromu Nakano. All rights reserved.
 
-mkdir Incomes/Configuration/Package/Firebase/
-echo $GOOGLESERVICE_BASE64 | base64 -d -o Incomes/Configuration/Package/Firebase/GoogleService-Info.plist
-echo $SECRET_BASE64 | base64 -d -o Incomes/Configuration/Secret.swift
-echo $STOREKIT_BASE64 | base64 -d -o Incomes/Configuration/Package/StoreKit/StoreKitTestCertificate.cer
+mkdir ../Incomes/Configuration/Package/Firebase/
+echo $GOOGLESERVICE_BASE64 | base64 -d -o ../Incomes/Configuration/Package/Firebase/GoogleService-Info.plist
+echo $SECRET_BASE64 | base64 -d -o ../Incomes/Configuration/Secret.swift
+echo $STOREKIT_BASE64 | base64 -d -o ../Incomes/Configuration/Package/StoreKit/StoreKitTestCertificate.cer
