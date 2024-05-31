e2a9133ca45db08afb25aea5b87e4fe8af2f837a
muhiro_ <muhiro.12@gmail.com>
2024-05-31 13:12:39 +0900

    Add notification to setting

--- Incomes/Presentation/View/SettingsView.swift
+++ Incomes/Presentation/View/SettingsView.swift
@@ -23,6 +23,24 @@ struct SettingsView {
 extension SettingsView: View {
     var body: some View {
         Form {
+            Section("Notification") {
+                HStack {
+                    Image(systemName: "exclamationmark.triangle")
+                    Text("Upcoming Changes to the Subscription Plan")
+                }
+                .font(.headline)
+                .foregroundStyle(.red)
+                Text(
+                    """
+                    We would like to inform you that starting August 1st (tentative), the contents of the subscription plan will be updated as follows:
+
+                      1. iCloud synchronization and ad removal will become subscription features.
+                      2. Features such as categories and graphs, which were previously part of the subscription, will now be available for free to all users.
+
+                    We greatly appreciate your support and understanding as we make these changes to enhance your experience with our app. Thank you for being a valued member of our community.
+                    """
+                )
+            }
             if isSubscribeOn {
                 PremiumSection()
             } else {
