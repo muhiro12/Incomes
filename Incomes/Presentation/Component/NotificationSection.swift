0fc04eac6016ad9bb33d4a7315e08fded90e59a7
muhiro_ <muhiro.12@gmail.com>
2024-06-03 12:57:06 +0900

    Add NotificationSection

---
+++ Incomes/Presentation/Component/NotificationSection.swift
@@ -0,0 +1,44 @@
+//
+//  NotificationSection.swift
+//  Incomes
+//
+//  Created by Hiromu Nakano on 2024/06/03.
+//  Copyright © 2024 Hiromu Nakano. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationSection: View {
+    @Environment(NotificationService.self)
+    private var notificationService
+
+    var body: some View {
+        Section("Notification") {
+            HStack {
+                Image(systemName: "exclamationmark.triangle")
+                Text("Upcoming Changes to the Subscription Plan")
+            }
+            .font(.headline)
+            .foregroundStyle(.red)
+            Text(
+                // swiftlint:disable line_length
+                """
+                We would like to inform you that starting August 1st (tentative), the contents of the subscription plan will be updated as follows:
+
+                  1. iCloud synchronization and ad removal will become subscription features.
+                  2. Features such as categories and graphs, which were previously part of the subscription, will now be available for free to all users.
+
+                We greatly appreciate your support and understanding as we make these changes to enhance your experience with our app. Thank you for being a valued member of our community.
+                """
+                // swiftlint:enable line_length
+            )
+        }
+        .task {
+            notificationService.refresh()
+        }
+    }
+}
+
+#Preview {
+    NotificationSection()
+}
