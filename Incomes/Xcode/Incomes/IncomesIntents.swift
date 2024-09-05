5a9ff297a90d095c669075e83db2b70f2e15e8e7
muhiro_ <muhiro.12@gmail.com>
2024-09-05 22:28:20 +0900

    Fix bug that AppIntents did not appear

--- Incomes/Xcode/Incomes/IncomesIntents.swift
+++ Incomes/Xcode/Incomes/IncomesIntents.swift
@@ -1,9 +1,17 @@
+//
+//  IncomesIntents.swift
+//  Incomes
+//
+//  Created by Hiromu Nakano on 9/5/24.
+//  Copyright © 2024 Hiromu Nakano. All rights reserved.
+//
+
 import AppIntents
 
 struct OpenIncomesIntent: AppIntent {
     static var title = LocalizedStringResource("Open Incomes")
     static var openAppWhenRun = true
-    
+
     func perform() async throws -> some IntentResult {
         .result()
     }
