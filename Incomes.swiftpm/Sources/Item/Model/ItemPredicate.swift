3ce63a05bc0d6530974196f115012fc9541b2290
muhiro_ <muhiro.12@gmail.com>
2024-10-01 01:08:27 +0900

    Rename Item/Tag Predicate

---
+++ Incomes.swiftpm/Sources/Item/Model/ItemPredicate.swift
@@ -0,0 +1,94 @@
+//
+//  ItemPredicate.swift
+//  Incomes
+//
+//  Created by Hiromu Nakano on 2021/12/29.
+//  Copyright © 2021 Hiromu Nakano. All rights reserved.
+//
+
+import Foundation
+import SwiftData
+
+enum ItemPredicate {
+    case all
+    case none
+    case dateIsBetween(start: Date, end: Date)
+    case dateIsAfter(Date)
+    case dateIsBefore(Date)
+    case dateIsSameDayAs(Date)
+    case dateIsSameMonthAs(Date)
+    case dateIsSameYearAs(Date)
+    case contentAndYear(content: String, year: String)
+    case repeatIDIs(UUID)
+    case repeatIDAndDateIsAfter(repeatID: UUID, date: Date)
+
+    var value: Predicate<Item> {
+        switch self {
+        case .all:
+            return .true
+        case .none:
+            return .false
+        case .dateIsBetween(let start, let end):
+            return #Predicate {
+                start <= $0.date && $0.date <= end
+            }
+        case .dateIsAfter(let date):
+            return Self.dateIsBetween(
+                start: Calendar.utc.startOfDay(for: date),
+                end: .distantFuture
+            ).value
+        case .dateIsBefore(let date):
+            return Self.dateIsBetween(
+                start: .distantPast,
+                end: Calendar.utc.startOfDay(for: date) - 1
+            ).value
+        case .dateIsSameDayAs(let date):
+            return Self.dateIsBetween(
+                start: Calendar.utc.startOfDay(for: date),
+                end: Calendar.utc.endOfDay(for: date)
+            ).value
+        case .dateIsSameMonthAs(let date):
+            return Self.dateIsBetween(
+                start: Calendar.utc.startOfMonth(for: date),
+                end: Calendar.utc.endOfMonth(for: date)
+            ).value
+        case .dateIsSameYearAs(let date):
+            return Self.dateIsBetween(
+                start: Calendar.utc.startOfYear(for: date),
+                end: Calendar.utc.endOfYear(for: date)
+            ).value
+        case .contentAndYear(let content, let year):
+            guard let date = year.dateValueWithoutLocale(.yyyy) else {
+                assertionFailure()
+                return .false
+            }
+            let start = Calendar.utc.startOfYear(for: date)
+            let end = Calendar.utc.endOfYear(for: date)
+            return #Predicate {
+                $0.content == content
+                    && start <= $0.date && $0.date <= end
+            }
+        case .repeatIDIs(let repeatID):
+            return #Predicate {
+                $0.repeatID == repeatID
+            }
+        case .repeatIDAndDateIsAfter(let repeatID, let date):
+            return #Predicate {
+                $0.repeatID == repeatID && $0.date >= date
+            }
+        }
+    }
+}
+
+extension FetchDescriptor where T == Item {
+    static func items(_ predicate: ItemPredicate, order: SortOrder = .reverse) -> FetchDescriptor {
+        .init(
+            predicate: predicate.value,
+            sortBy: [
+                .init(\.date, order: order),
+                .init(\.content, order: order),
+                .init(\.persistentModelID, order: order)
+            ]
+        )
+    }
+}
---
+++ Incomes.swiftpm/Sources/Tag/Model/TagPredicate.swift
@@ -0,0 +1,80 @@
+//
+//  TagPredicate.swift
+//  Incomes
+//
+//  Created by Hiromu Nakano on 2023/09/10.
+//  Copyright © 2023 Hiromu Nakano. All rights reserved.
+//
+
+import Foundation
+import SwiftData
+
+enum TagPredicate {
+    case all
+    case none
+    case idIs(Tag.ID)
+    case isSameWith(Tag)
+    case nameAndType(name: String, type: Tag.TagType)
+    case typeIs(Tag.TagType)
+    case yearIs(String)
+    case dateIsSameMonthAs(Date)
+    case contentIsIn([String])
+
+    var value: Predicate<Tag> {
+        switch self {
+        case .all:
+            return .true
+        case .none:
+            return .false
+        case .idIs(let id):
+            return #Predicate {
+                $0.persistentModelID == id
+            }
+        case .isSameWith(let tag):
+            let name = tag.name
+            let typeID = tag.typeID
+            return #Predicate {
+                $0.name == name && $0.typeID == typeID
+            }
+        case .nameAndType(name: let name, type: let type):
+            let id = type.rawValue
+            return #Predicate {
+                $0.name == name && $0.typeID == id
+            }
+        case .typeIs(let type):
+            let id = type.rawValue
+            return #Predicate {
+                $0.typeID == id
+            }
+        case .yearIs(let year):
+            let id = Tag.TagType.yearMonth.rawValue
+            return #Predicate {
+                $0.name.starts(with: year) && $0.typeID == id
+            }
+        case .dateIsSameMonthAs(let date):
+            let name = date.stringValueWithoutLocale(.yyyyMM)
+            let id = Tag.TagType.yearMonth.rawValue
+            return #Predicate {
+                $0.name == name && $0.typeID == id
+            }
+        case .contentIsIn(let contents):
+            let id = Tag.TagType.content.rawValue
+            return #Predicate {
+                contents.contains($0.name) && $0.typeID == id
+            }
+        }
+    }
+}
+
+extension FetchDescriptor where T == Tag {
+    static func tags(_ predicate: TagPredicate, order: SortOrder = .forward) -> FetchDescriptor {
+        .init(
+            predicate: predicate.value,
+            sortBy: [
+                .init(\.name, order: order),
+                .init(\.typeID, order: order),
+                .init(\.persistentModelID, order: order)
+            ]
+        )
+    }
+}
