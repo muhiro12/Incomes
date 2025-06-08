28fb77ba35e3456e4632e876609fc6a088f1d89c
muhiro_ <58671793+muhiro12@users.noreply.github.com>
2025-06-08 13:29:07 +0900

    Refactor navigation usage

--- Incomes/Sources/Item/Component/RecalculateButton.swift
+++ Incomes/Sources/Item/Component/RecalculateButton.swift
@@ -2,9 +2,6 @@ import SwiftUI
 import SwiftUtilities
 
 struct RecalculateButton: View {
-    @Environment(ItemService.self)
-    private var itemService
-
     @State private var selectedDate = Calendar.current.startOfMonth(for: .now)
     @State private var isDialogPresented = false
     @State private var isRecalculating = false
@@ -16,50 +13,79 @@ struct RecalculateButton: View {
             Text("Recalculate")
         }
         .sheet(isPresented: $isDialogPresented) {
-            NavigationStack {
-                List {
-                    Section {
-                        DatePicker(
-                            selection: $selectedDate,
-                            displayedComponents: .date
-                        ) {
-                            Text("Choose a Date")
+            RecalculateNavigationView(
+                selectedDate: $selectedDate,
+                isRecalculating: $isRecalculating,
+                isDialogPresented: $isDialogPresented
+            )
+        }
+    }
+}
+
+struct RecalculateNavigationView: View {
+    @Binding var selectedDate: Date
+    @Binding var isRecalculating: Bool
+    @Binding var isDialogPresented: Bool
+
+    var body: some View {
+        NavigationStack {
+            RecalculateView(
+                selectedDate: $selectedDate,
+                isRecalculating: $isRecalculating,
+                isDialogPresented: $isDialogPresented
+            )
+        }
+    }
+}
+
+private struct RecalculateView: View {
+    @Environment(ItemService.self) private var itemService
+    @Binding var selectedDate: Date
+    @Binding var isRecalculating: Bool
+    @Binding var isDialogPresented: Bool
+
+    var body: some View {
+        List {
+            Section {
+                DatePicker(
+                    selection: $selectedDate,
+                    displayedComponents: .date
+                ) {
+                    Text("Choose a Date")
+                }
+                .datePickerStyle(.graphical)
+                Button {
+                    Task {
+                        isRecalculating = true
+                        do {
+                            try itemService.recalculate(after: selectedDate)
+                            try await Task.sleep(for: .seconds(5))
+                        } catch {
+                            assertionFailure(error.localizedDescription)
                         }
-                        .datePickerStyle(.graphical)
-                        Button {
-                            Task {
-                                isRecalculating = true
-                                do {
-                                    try itemService.recalculate(after: selectedDate)
-                                    try await Task.sleep(for: .seconds(5))
-                                } catch {
-                                    assertionFailure(error.localizedDescription)
-                                }
-                                isRecalculating = false
-                                isDialogPresented = false
-                            }
-                        } label: {
-                            HStack {
-                                Text("Recalculate")
-                                Spacer()
-                                if isRecalculating {
-                                    ProgressView()
-                                }
-                            }
+                        isRecalculating = false
+                        isDialogPresented = false
+                    }
+                } label: {
+                    HStack {
+                        Text("Recalculate")
+                        Spacer()
+                        if isRecalculating {
+                            ProgressView()
                         }
-                    } header: {
-                        Text("Choose a Date")
-                    } footer: {
-                        Text("Data after this date will be recalculated.")
                     }
                 }
-                .toolbar {
-                    CloseButton()
-                }
-                .disabled(isRecalculating)
-                .interactiveDismissDisabled()
+            } header: {
+                Text("Choose a Date")
+            } footer: {
+                Text("Data after this date will be recalculated.")
             }
         }
+        .toolbar {
+            CloseButton()
+        }
+        .disabled(isRecalculating)
+        .interactiveDismissDisabled()
     }
 }
 
