ebef168d01f873a32ec1d2592c8b8cd90272ace2
muhiro_ <muhiro.12@gmail.com>
2025-09-10 04:56:34 +0900

    Add IncomesLibrary to WidgetsExtension

---
+++ Widgets/IncomesLibrary.swift
@@ -0,0 +1,9 @@
+//
+//  IncomesLibrary.swift
+//  Incomes
+//
+//  Created by Hiromu Nakano on 2025/09/10.
+//  Copyright © 2025 Hiromu Nakano. All rights reserved.
+//
+
+@_exported import IncomesLibrary
