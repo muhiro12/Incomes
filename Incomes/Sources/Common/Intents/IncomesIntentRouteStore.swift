520020cb332e56c049e7fb4056c6658c0a56925e
muhiro_ <muhiro.12@gmail.com>
2026-03-07 11:17:08 +0900

    Simplify deep-link glue with MHPlatform helpers

--- Incomes/Sources/Common/Intents/IncomesIntentRouteStore.swift
+++ Incomes/Sources/Common/Intents/IncomesIntentRouteStore.swift
@@ -3,15 +3,10 @@ import MHPlatform
 
 enum IncomesIntentRouteStore {
     private static let pendingDeepLinkURLKey = "pendingIntentDeepLinkURL"
-    private static var deepLinkStore: MHDeepLinkStore? {
-        guard let userDefaults = UserDefaults(suiteName: AppGroup.id) else {
-            return nil
-        }
-        return .init(
-            userDefaults: userDefaults,
-            key: pendingDeepLinkURLKey
-        )
-    }
+    private static let deepLinkStore = MHDeepLinkStore(
+        suiteName: AppGroup.id,
+        key: pendingDeepLinkURLKey
+    )
 
     static func store(_ url: URL) {
         deepLinkStore?.ingest(url)
