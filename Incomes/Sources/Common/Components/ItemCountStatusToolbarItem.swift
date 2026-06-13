30795ae6fcb26eae744c65e62c08704bf1f2fbfa
muhiro_ <muhiro.12@gmail.com>
2026-06-13 23:50:20 +0900

    Fix item count status pluralization

--- Incomes/Sources/Common/Components/ItemCountStatusToolbarItem.swift
+++ Incomes/Sources/Common/Components/ItemCountStatusToolbarItem.swift
@@ -1,11 +1,19 @@
 import SwiftUI
 
 struct ItemCountStatusToolbarItem: ToolbarContent {
+    private enum Constants {
+        static let singleItemCount = 1
+    }
+
     let count: Int
 
     var body: some ToolbarContent {
         StatusToolbarItem {
-            Text("\(count, format: .number) Items")
+            if count == Constants.singleItemCount {
+                Text("\(count, format: .number) Item")
+            } else {
+                Text("\(count, format: .number) Items")
+            }
         }
     }
 }
