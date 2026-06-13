aad36193fac5840375eb84abc64d11037120eaec
muhiro_ <muhiro.12@gmail.com>
2026-06-14 04:32:47 +0900

    Adapt item rows for large text

--- Incomes/Sources/Item/Components/ListItemButtonLabel.swift
+++ Incomes/Sources/Item/Components/ListItemButtonLabel.swift
@@ -1,12 +1,14 @@
 import SwiftUI
 
 struct ListItemButtonLabel: View {
+    @Environment(\.dynamicTypeSize)
+    private var dynamicTypeSize
     @Environment(\.horizontalSizeClass)
     private var horizontalSizeClass
 
     var body: some View {
         NavigationRowLabel {
-            if horizontalSizeClass == .regular {
+            if horizontalSizeClass == .regular, !dynamicTypeSize.isAccessibilitySize {
                 WideListItem()
             } else {
                 NarrowListItem()
