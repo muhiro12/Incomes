f42f4802ce03b96100fe0827839c965ca31a5fab
muhiro_ <muhiro.12@gmail.com>
2026-06-24 22:25:36 +0900

    Update package pins

--- IncomesLibrary/Package.swift
+++ IncomesLibrary/Package.swift
@@ -18,7 +18,7 @@ let package = Package( // swiftlint:disable:this prefixed_toplevel_constant
     ],
     dependencies: [
         .package(
-            url: "https://github.com/muhiro12/MHPlatform.git",
+            url: "https://github.com/muhiro12/MHPlatform",
             "1.0.0"..<"2.0.0"
         )
     ],
